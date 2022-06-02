326630f065899b7bbb98338155d6d3f89bd7acc2
Jose Cruz <jose.cruz@bdp.com.bo>
2022-06-02 16:57:45 -0400

    Vistas y controladores

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -25,19 +25,29 @@ class FormController extends Controller
     public function storeProd(Request $data) {
         $producto = new Producto();
 
-        $producto->nombre_producto = $data->nombre_producto;
-        $producto->descripcion_producto = $data->descripcion_producto;
+        if ($data->hasFile('marca_logo_empresa')) {
+            $fileLogo = $data->file('marca_logo_empresa');
+            $endPathLogo = 'images/productos/';
+            $fileNameLogo = time() . '-' . $fileLogo->getClientOriginalName();
+            $uploadSuccessLogo = $data->file('marca_logo_empresa')->move($endPathLogo, $fileNameLogo);
+            $producto->marca_logo_empresa = $endPathLogo . $fileNameLogo;
+        }
+        $producto->descripcion_empresa = $data->descripcion_empresa;
+        $producto->rubro_producto = $data->rubro_producto;
+        $producto->numero_producto = $data->numero_producto;
         $producto->categoria_producto = $data->categoria_producto;
-        $producto->precio_producto = $data->precio_producto;
-        $producto->moneda_producto = $data->moneda_producto;
-
+        $producto->nombre_producto = $data->nombre_producto;
         if ($data->hasFile('imagen_producto')) {
             $file = $data->file('imagen_producto');
             $endPath = 'images/productos/';
-            $filename = time() . '-' . $file->getClientOriginalName();
-            $uploadSuccess = $data->file('imagen_producto')->move($endPath, $filename);
-            $producto->binario_producto = $endPath . $filename;
+            $fileName = time() . '-' . $file->getClientOriginalName();
+            $uploadSuccess = $data->file('imagen_producto')->move($endPath, $fileName);
+            $producto->binario_producto = $endPath . $fileName;
         }
+        $producto->unidad_medida_producto = $data->unidad_medida_producto;
+        $producto->descripcion_producto = $data->descripcion_producto;
+        $producto->precio_producto = $data->precio_producto;
+        $producto->moneda_producto = $data->moneda_producto;
 
         $producto->save();
         return redirect()->to('/');
---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductoController extends Controller
+{
+    public function index() {
+        $productos = DB::table('productos')->get();
+        return view('vistas.ver_productos', ['productos' => $productos]);
+    }
+}
