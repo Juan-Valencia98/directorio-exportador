2c3e035f92a3c802265a512482c01be51414a935
Juan Valencia <67487522+Juan-Valencia98@users.noreply.github.com>
2022-06-06 21:59:10 -0400

    agregando sedeers

--- database/seeders/RubroSeeder.php
+++ database/seeders/RubroSeeder.php
@@ -22,5 +22,17 @@ class RubroSeeder extends Seeder
             'nombre_rubro' => 'PIEZAS METÁLICAS',
             'descripcion_rubro' => 'Campo de la fabriación de metales',
         ]);
+        Rubro::create([
+            'nombre_rubro' => 'PIEZAS METÁLICAS',
+            'descripcion_rubro' => 'Campo de la fabriación de metales',
+        ]);
+        Rubro::create([
+            'nombre_rubro' => 'PIEZAS METÁLICAS',
+            'descripcion_rubro' => 'Campo de la fabriación de metales',
+        ]);
+        Rubro::create([
+            'nombre_rubro' => 'PIEZAS METÁLICAS',
+            'descripcion_rubro' => 'Campo de la fabriación de metales',
+        ]);
     }
 }
